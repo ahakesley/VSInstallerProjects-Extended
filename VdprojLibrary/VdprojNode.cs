fcfbcd9b70df4724196bf986571323f39d94608a
ahakesley95 <35697089+ahakesley@users.noreply.github.com>
2025-10-30 21:36:22 +0000

    Changed type of VdprojNode Properties property back to Dictionary<string, string>

--- VdprojLibrary/VdprojNode.cs
+++ VdprojLibrary/VdprojNode.cs
@@ -5,15 +5,9 @@ namespace SetupProjectHelper.VdprojLibrary;
 public class VdprojNode
 {
     public string Name { get; set; } = string.Empty;
-    public Dictionary<string, VdprojProperty> Properties { get; set; } = new();
+    public Dictionary<string, string> Properties { get; set; } = new();
     public List<VdprojNode> Children { get; set; } = new();
 
     public override string ToString() => Name;
     
-}
-
-public class VdprojProperty
-{
-    public string Value { get; set; } = string.Empty;
-    public VdprojNode? SubProperty { get; set; } = null;
 }
